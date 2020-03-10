a519f157fb541b69c5fca0a47e512d8126daf364
Manuela Manolova <mmanolova13@gmail.com>
2020-03-10 15:31:23 +0100

    connect to the iex api

--- index.js
+++ index.js
@@ -1,20 +1,56 @@
 const express = require('express');
 const exphbs  = require('express-handlebars');
+const request = require('request');
 const path = require('path');
-
+const bodyparser = require('body-parser')
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+
+
+// User body parser middleware
+app.use(bodyparser.urlencoded({ extended: false }));
+
+// API Token: pk_f2631b6f06d54b13b7f0caf99d23bd8a
+function call_api(finishedAPI, ticker) {
+  request('https://cloud.iexapis.com/stable/stock/' + ticker + '/quote?token=pk_f2631b6f06d54b13b7f0caf99d23bd8a', { json: true }, (err, res, body) => {
+    if (err) {
+      console.log(err);
+    }
+
+    if (res.statusCode === 200) {
+      finishedAPI(body);
+    }
+  });
+}
 
 // Set handlebars middleware
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
-// Set handlebar routing
+// Set handlebar index GET route
 app.get('/', function (req, res) {
+  call_api(function(doneAPI) {
     res.render('home', {
-      stuff: 'this is stuff'
+      stock: doneAPI
     });
+  }, "fb");
+});
+
+// Set handlebar index POST route
+app.post('/', function (req, res) {
+  call_api(function(doneAPI) {
+    //posted_stock = req.body.stock_ticker;
+    res.render('home', {
+      stock: doneAPI,
+      //posted_stock: posted_stock
+    });
+  }, req.body.stock_ticker);
+});
+
+// Create about page route
+app.get('/about.html', function (req, res) {
+    res.render('about');
 });
 
 // Set static folder
